11f978be90bfd68fb073a23f37c9055d8d5079b6
Andrew Kvapil <git@viluon.me>
2024-08-09 23:39:16 +0200

    Adopt treefmt

--- flake.nix
+++ flake.nix
@@ -7,19 +7,38 @@
 
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+      ];
+
       flake = {
         nixosConfigurations.nixluon = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [ ./configuration.nix ];
         };
       };
+
       systems = [
         # systems for which you want to build the `perSystem` attributes
         "x86_64-linux"
       ];
-      perSystem = { config, ... }: {
+
+      perSystem = { config, pkgs, ... }: {
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          # formats .nix files
+          programs.nixpkgs-fmt.enable = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            config.treefmt.build.wrapper
+          ] ++ (builtins.attrValues config.treefmt.build.programs);
+        };
       };
     };
 }

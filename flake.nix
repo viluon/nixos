a96be9fdcdde8e0a679c39c9b22228a2c491dda5
Andrew Kvapil <git@viluon.me>
2024-08-09 23:21:41 +0200

    Adopt flake-parts

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.nixluon = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        nixosConfigurations.nixluon = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+      systems = [
+        # systems for which you want to build the `perSystem` attributes
+        "x86_64-linux"
+      ];
+      perSystem = { config, ... }: {
+      };
     };
-  };
 }

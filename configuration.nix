52e5a5712c85df3360fb55bf0849c589c550711d
Andrew Kvapil <git@viluon.me>
2024-10-23 19:50:41 +0200

    Add nix-community cachix

--- configuration.nix
+++ configuration.nix
@@ -19,17 +19,19 @@
   nix.daemonCPUSchedPolicy = "idle";
   nix.daemonIOSchedClass = "idle";
 
-  # trust viluon to configure binary caches
   nix.settings = {
+    # trust viluon to configure binary caches
     trusted-users = [ "root" "viluon" ];
 
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
       "https://viluon.cachix.org"
     ];
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "viluon.cachix.org-1:lYmQCd3Bb0GYrHCxPCshn2oCRDMqMmN/i5kgkBlxmNk="
     ];
   };

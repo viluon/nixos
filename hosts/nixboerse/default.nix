f04a317f51f9025dab62137e9820450fbcabc4d7
Ondřej Kvapil <git@viluon.me>
2025-06-27 17:38:52 +0200

    Enable NVIDIA drivers

--- hosts/nixboerse/default.nix
+++ hosts/nixboerse/default.nix
@@ -80,6 +80,9 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";

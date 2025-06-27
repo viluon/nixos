45649682552f3b9d64d6049c4a5df5dd5e68d9d3
Ondřej Kvapil <git@viluon.me>
2025-06-27 15:30:16 +0200

    Extract Nix configuration

--- hosts/nixluon/default.nix
+++ hosts/nixluon/default.nix
@@ -19,45 +19,13 @@
       ./hardware.nix
       ../../modules/system/monitoring/governor-control.nix
       ../../modules/system/monitoring/grafana-config.nix
+      ../../modules/system/nix
     ];
 
-  nix = {
-    # nix version
-    package = pkgs.nixStable;
-
-    # set nix build scheduling policies to idle to preserve interactivity
-    daemonCPUSchedPolicy = "idle";
-    daemonIOSchedClass = "idle";
-
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-
-      # trust viluon to configure binary caches
-      trusted-users = [ "root" "viluon" ];
-
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://viluon.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "viluon.cachix.org-1:lYmQCd3Bb0GYrHCxPCshn2oCRDMqMmN/i5kgkBlxmNk="
-      ];
-    };
-
-    gc = {
-      automatic = true;
-      randomizedDelaySec = "15min";
-      options = "--delete-older-than 60d";
-    };
-
-    optimise = {
-      automatic = true;
-      dates = [ "20:00" ];
-    };
+  nix.gc = {
+    automatic = true;
+    randomizedDelaySec = "15min";
+    options = "--delete-older-than 60d";
   };
 
   # kernel version
---
+++ modules/system/nix/default.nix
@@ -0,0 +1,39 @@
+{ config
+, pkgs
+, ...
+}:
+
+{
+  nix = {
+    # nix version
+    package = pkgs.nixStable;
+
+    # set nix build scheduling policies to idle to preserve interactivity
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedClass = "idle";
+
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+
+      # trust viluon to configure binary caches
+      trusted-users = [ "root" "viluon" ];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://viluon.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "viluon.cachix.org-1:lYmQCd3Bb0GYrHCxPCshn2oCRDMqMmN/i5kgkBlxmNk="
+      ];
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [ "20:00" ];
+    };
+  };
+}

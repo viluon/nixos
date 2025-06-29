dc6642da8ce0b764562cc8a246e9196f5b7586b6
Ondřej Kvapil <git@viluon.me>
2025-06-29 15:39:21 +0200

    Adopt ML4W Hyprland

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     flake-root.url = "github:srid/flake-root";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
+    ml4w-dotfiles.url = "github:mylinuxforwork/dotfiles";
+    ml4w-dotfiles.flake = false;
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     nix4vscode.url = "github:nix-community/nix4vscode";
@@ -59,6 +61,7 @@
               home-manager.extraSpecialArgs = {
                 inherit inputs hostname;
                 unstable-pkgs = nixpkgs-unstable.legacyPackages.${config.system};
+                ml4w-dotfiles = inputs.ml4w-dotfiles;
               };
             }
           ];
--- home/viluon/home.nix
+++ home/viluon/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, inputs, hostname ? "unknown", ... }:
+{ config, pkgs, lib, inputs, hostname, ml4w-dotfiles, ... }:
 
 let
   commonPackages = with pkgs; [
@@ -10,6 +10,44 @@ let
     ripgrep
     fd
     vivid
+
+    # Hyprland ecosystem
+    waybar
+    hyprpaper
+    hypridle
+    hyprlock
+    hyprpicker
+    dunst
+    rofi-wayland
+    wlogout
+
+    # System utilities
+    wl-clipboard
+    cliphist
+    playerctl
+    brightnessctl
+    pavucontrol
+    networkmanager
+    blueman
+    polkit_gnome
+
+    # File manager and utilities
+    xfce.thunar
+    xfce.tumbler # Thumbnails for thunar
+
+    # Fonts
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-cjk-sans
+    font-awesome
+
+    # ML4W specific tools
+    figlet
+    gum
+    matugen
+    wallust
+    swww
+    xdg-user-dirs
   ];
 
   # Host-specific packages
@@ -107,16 +145,42 @@ in
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # Copy ML4W Hyprland scripts
+    ".config/hypr/scripts" = {
+      source = "${ml4w-dotfiles}/share/dotfiles/.config/hypr/scripts";
+      recursive = true;
+      executable = true;
+    };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    # Copy ML4W Waybar configuration
+    ".config/waybar" = {
+      source = "${ml4w-dotfiles}/share/dotfiles/.config/waybar";
+      recursive = true;
+    };
+
+    # Copy ML4W Rofi configuration
+    ".config/rofi" = {
+      source = "${ml4w-dotfiles}/share/dotfiles/.config/rofi";
+      recursive = true;
+    };
+
+    # Copy ML4W Dunst configuration
+    ".config/dunst" = {
+      source = "${ml4w-dotfiles}/share/dotfiles/.config/dunst";
+      recursive = true;
+    };
+
+    # Copy ML4W wlogout configuration
+    ".config/wlogout" = {
+      source = "${ml4w-dotfiles}/share/dotfiles/.config/wlogout";
+      recursive = true;
+    };
+
+    # Copy ML4W wallpapers
+    ".local/share/wallpapers" = {
+      source = "${ml4w-dotfiles}/share/wallpapers";
+      recursive = true;
+    };
   };
 
   programs.bash = {
@@ -231,26 +295,263 @@ in
     nix-direnv.enable = true;
   };
 
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      # Monitor configuration
+      monitor = [
+        ",preferred,auto,auto"
+      ];
+
+      # Environment variables
+      env = [
+        "CLUTTER_BACKEND,wayland"
+        "GDK_BACKEND,wayland,x11,*"
+        "HYPRCURSOR_SIZE,24"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "SDL_VIDEODRIVER,wayland"
+        "XCURSOR_SIZE,24"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+      ];
+
+      # Input configuration
+      input = {
+        kb_layout = "us";
+        kb_variant = "";
+        kb_model = "";
+        kb_options = "caps:swapescape";
+        kb_rules = "";
+
+        follow_mouse = 1;
+        sensitivity = 0;
+
+        touchpad = {
+          natural_scroll = true;
+          disable_while_typing = true;
+          tap-to-click = true;
+        };
+      };
+
+      # General configuration
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+
+      # Decoration settings
+      decoration = {
+        rounding = 10;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+
+      # Animations
+      animations = {
+        enabled = true;
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      # Dwindle layout
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      # Master layout
+      master = {
+        new_status = "master";
+      };
+
+      # Misc settings
+      misc = {
+        force_default_wallpaper = -1;
+        disable_hyprland_logo = false;
+      };
+
+      # Variables
+      "$terminal" = "kitty";
+      "$fileManager" = "thunar";
+      "$menu" = "rofi -show drun";
+      "$mod" = "SUPER";
+
+      # Key bindings
+      bind = [
+        # Applications
+        "$mod, Q, exec, $terminal"
+        "$mod, C, killactive,"
+        "$mod, M, exit,"
+        "$mod, E, exec, $fileManager"
+        "$mod, V, togglefloating,"
+        "$mod, R, exec, $menu"
+        "$mod, P, pseudo," # dwindle
+        "$mod, J, togglesplit," # dwindle
         "$mod, F, exec, firefox"
+
+        # Move focus with mainMod + arrow keys
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        # Move focus with mainMod + hjkl
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        # Special workspace (scratchpad)
+        "$mod, S, togglespecialworkspace, magic"
+        "$mod SHIFT, S, movetoworkspace, special:magic"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up, workspace, e-1"
+
+        # Screenshot - ML4W specific bindings
         ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList
-          (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
+        "$mod, Print, exec, grimblast copy screen"
+        "$mod SHIFT, W, exec, ~/.config/hypr/scripts/wallpaper.sh"
+        "$mod SHIFT, B, exec, waybar"
+        "$mod CTRL, Q, exec, wlogout"
+
+        # Window management
+        "$mod SHIFT, left, movewindow, l"
+        "$mod SHIFT, right, movewindow, r"
+        "$mod SHIFT, up, movewindow, u"
+        "$mod SHIFT, down, movewindow, d"
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, l, movewindow, r"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, j, movewindow, d"
+
+        # Resize windows
+        "$mod CTRL, left, resizeactive, -20 0"
+        "$mod CTRL, right, resizeactive, 20 0"
+        "$mod CTRL, up, resizeactive, 0 -20"
+        "$mod CTRL, down, resizeactive, 0 20"
+        "$mod CTRL, h, resizeactive, -20 0"
+        "$mod CTRL, l, resizeactive, 20 0"
+        "$mod CTRL, k, resizeactive, 0 -20"
+        "$mod CTRL, j, resizeactive, 0 20"
+
+        # ML4W specific bindings
+        "$mod SHIFT, S, exec, ~/.config/hypr/scripts/hyprshade.sh"
+        "$mod CTRL, S, exec, ml4w-hyprland"
+        "$mod, G, exec, ~/.config/hypr/scripts/gamemode.sh"
+        "$mod ALT, G, exec, ~/.config/hypr/scripts/gtk.sh"
+        "$mod SHIFT, A, exec, ~/.config/hypr/scripts/toggleallfloat.sh"
+        "$mod, T, exec, ~/.config/hypr/scripts/toggleallfloat.sh"
+        "$mod ALT, F, exec, ~/.config/hypr/scripts/filemanager.sh"
+        "$mod CTRL, F, exec, ~/.config/hypr/scripts/filemanager.sh"
+
+        # Media keys
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+
+        # Brightness
+        ", XF86MonBrightnessUp, exec, brightnessctl set 10%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
+      ];
+
+      # Mouse bindings
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      # Window rules
+      windowrulev2 = [
+        "suppressevent maximize, class:.*"
+        "float, class:^(pavucontrol)$"
+        "float, class:^(blueman-manager)$"
+        "float, class:^(nm-connection-editor)$"
+        "float, class:^(gnome-calculator)$"
+        "float, title:^(Picture-in-Picture)$"
+        "pin, title:^(Picture-in-Picture)$"
+        "size 800 600, title:^(Picture-in-Picture)$"
+        "move 100%-820 100%-620, title:^(Picture-in-Picture)$"
+      ];
+
+      # Layer rules
+      layerrule = [
+        "blur, gtk-layer-shell"
+        "blur, rofi"
+        "ignorezero, rofi"
+      ];
+
+      # Autostart
+      exec-once = [
+        "waybar"
+        "hyprpaper"
+        "hypridle"
+        "dunst"
+        "nm-applet"
+        "blueman-applet"
+        "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+        "wl-paste --type text --watch cliphist store"
+        "wl-paste --type image --watch cliphist store"
+      ];
+    };
   };
 
   # Home Manager can also manage your environment variables through

095c125c48753b12c8914491ec4be041994bacac
Ondřej Kvapil <git@viluon.me>
2025-07-17 13:26:41 +0200

    Refactor libvirt hook

--- hosts/nixboerse/default.nix
+++ hosts/nixboerse/default.nix
@@ -53,20 +53,36 @@
   virtualisation.libvirtd = {
     enable = true;
     qemu.package = pkgs.qemu_kvm;
-    hooks.qemu = {
-      libvirtd-network-hook = pkgs.writeShellScript "libvirtd-network-hook" ''
-        set -euo pipefail
+    hooks.qemu =
+      let
+        domains = builtins.concatStringsSep " " [
+          "cedelgroup.com"
+          "cloudconsole-pa.clients6.google.com"
+          "cloudusersettings-pa.clients6.google.com"
+          "dbgcloud.io"
+          "deutsche-boerse.de"
+          "gke.goog"
+          "googleapis.com"
+          "oa.pnrad.net"
+        ];
 
-        case $1:$2 in
-          ubuntu:start)
-            ${pkgs.systemd}/bin/resolvectl dns virbr0 100.64.0.2
-            ${pkgs.systemd}/bin/resolvectl domain virbr0 deutsche-boerse.de oa.pnrad.net dbgcloud.io googleapis.com gke.goog cloudusersettings-pa.clients6.google.com cloudconsole-pa.clients6.google.com
-            ${pkgs.systemd}/bin/resolvectl default-route virbr0 no
-            ${pkgs.systemd}/bin/resolvectl dnsovertls virbr0 no
-            ;;
-        esac
-      '';
-    };
+        resolvectl = "${pkgs.systemd}/bin/resolvectl";
+        VM = "ubuntu";
+      in
+      {
+        libvirtd-network-hook = pkgs.writeShellScript "libvirtd-network-hook" ''
+          set -euo pipefail
+
+          case $1:$2 in
+            ${VM}:start)
+              ${resolvectl} dns virbr0 100.64.0.2
+              ${resolvectl} domain virbr0 ${domains}
+              ${resolvectl} default-route virbr0 no
+              ${resolvectl} dnsovertls virbr0 no
+              ;;
+          esac
+        '';
+      };
   };
 
   systemd.services.libvirtd.restartTriggers = [

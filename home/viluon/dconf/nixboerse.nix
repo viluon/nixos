c2d9964b83431b02f2b3db341dfd61dfe07193ec
Ondřej Kvapil <git@viluon.me>
2025-10-17 19:21:14 +0200

    Enable new extensions

--- home/viluon/dconf/nixboerse.nix
+++ home/viluon/dconf/nixboerse.nix
@@ -7,8 +7,11 @@ with lib.hm.gvariant;
 {
   "org/gnome/shell" = {
     enabled-extensions = [
+      "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
       "display-brightness-ddcutil@themightydeity.github.com"
       "kimpanel@kde.org"
+      "middleclickclose@paolo.tranquilli.gmail.com"
+      "Vitals@CoreCoding.com"
     ];
   };
 
@@ -16,6 +19,16 @@ with lib.hm.gvariant;
     mru-sources = [ (mkTuple [ "xkb" "us" ]) ];
     sources = [ (mkTuple [ "xkb" "us" ]) ];
     xkb-options = [ "lv3:ralt_switch" "compose:rctrl" ];
+  };
+
+  "org/gnome/shell/extensions/auto-move-windows" = {
+    application-list = [
+      "firefox.desktop:1"
+      "idea-ultimate.desktop:2"
+      "obsidian.desktop:3"
+      "org.gnome.SystemMonitor.desktop:3"
+      "virt-manager.desktop:4"
+    ];
   };
 
   "org/gnome/desktop/interface" = {

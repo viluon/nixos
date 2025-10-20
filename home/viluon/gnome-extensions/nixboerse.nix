17eea5c57dbee6b8249d624d6346276b4e13c070
Ondřej Kvapil <git@viluon.me>
2025-10-20 10:35:05 +0200

    Remove auto-move-windows

--- home/viluon/gnome-extensions/nixboerse.nix
+++ home/viluon/gnome-extensions/nixboerse.nix
@@ -2,7 +2,6 @@
 
 {
   gnome.extensions.enabledExtensions = with pkgs.gnomeExtensions; [
-    auto-move-windows
     brightness-control-using-ddcutil
     kimpanel
     middle-click-to-close-in-overview

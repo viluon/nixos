db842086f4e2beae606781ea5884ffdf622160b2
Ondřej Kvapil <git@viluon.me>
2025-10-30 15:48:49 +0100

    Adopt CachyOS kernel on nixboerse

--- hosts/nixboerse/kernel.nix
+++ hosts/nixboerse/kernel.nix
@@ -1,8 +1,20 @@
 { lib
+, pkgs
 , ...
 }:
 
+let
+  kernelPackages = pkgs.linuxPackages_cachyos-lto.cachyOverride { mArch = "GENERIC_V4"; };
+in
 {
+  boot.kernelPackages = kernelPackages;
+
+  # eBPF-based scheduler
+  services.scx.enable = true;
+
+  # workaround for 25.05, see https://github.com/chaotic-cx/nyx/issues/1158#issuecomment-3216945109
+  system.modulesTree = [ (lib.getOutput "modules" kernelPackages.kernel) ];
+
   boot.kernelPatches = [
     {
       name = "cgroups-v1-for-jvm";

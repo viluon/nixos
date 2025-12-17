0722a703f2c6ecdf0a618da2418af567a9dc9a18
Ondřej Kvapil <git@viluon.me>
2025-12-17 16:02:50 +0100

    Add CZ layout

--- modules/desktop/input-methods.nix
+++ modules/desktop/input-methods.nix
@@ -6,9 +6,36 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      settings = {
+        globalOptions = {
+          "Hotkey/TriggerKeys" = {
+            "0" = "Control+Alt+space";
+          };
+        };
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "keyboard-cz-qwerty";
+            Layout = "";
+          };
+          GroupOrder = {
+            "0" = "Default";
+          };
+        };
+      };
+    };
   };
 }

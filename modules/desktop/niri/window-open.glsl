2faac6f2401188d11e87968fca085f95b22ee2d0
Ondřej Kvapil <git@viluon.me>
2025-12-23 09:14:23 +0100

    Improve window open animation

--- modules/desktop/niri/window-open.glsl
+++ modules/desktop/niri/window-open.glsl
@@ -5,10 +5,14 @@ vec4 open_color(vec3 coords_geo, vec3 size_geo) {
 
   float wave_delay = coords_geo.y * coords_geo.y * coords_geo.y * 0.9;
   float local_p = clamp((p - wave_delay) / (1.0 - wave_delay), 0.0, 1.0);
-  float scale_x = max(0.01, mix(0.05, 1.0, local_p));
+  if (local_p <= 0.001) {
+    return vec4(0.0);
+  }
+
+  float scale_x = max(0.0001, mix(0.00, 1.0, local_p));
 
   float offset_x = (coords_geo.x - 0.5) / scale_x + 0.5;
-  float offset_y = (coords_geo.y - 0.5) / scale_y + 0.5;
+  float offset_y = mix(-0.5, 0.0, p) + (coords_geo.y - 0.5) / scale_y + 0.5;
 
   vec3 coords_tex = niri_geo_to_tex * vec3(offset_x, offset_y, 1.0);
   vec4 color = texture2D(niri_tex, coords_tex.st);

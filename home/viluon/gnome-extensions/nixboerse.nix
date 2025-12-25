118e55b93eb28c23a953fd22fdd88f30768734cb
Ondřej Kvapil <git@viluon.me>
2025-12-25 16:12:27 +0100

    Remove auto-move-windows

--- home/viluon/gnome-extensions/nixboerse.nix
+++ home/viluon/gnome-extensions/nixboerse.nix
@@ -7,14 +7,4 @@
     middle-click-to-close-in-overview
     vitals
   ];
-
-  dconf.settings."org/gnome/shell/extensions/auto-move-windows" = {
-    application-list = [
-      "firefox.desktop:1"
-      "idea-ultimate.desktop:2"
-      "obsidian.desktop:3"
-      "org.gnome.SystemMonitor.desktop:3"
-      "virt-manager.desktop:4"
-    ];
-  };
 }

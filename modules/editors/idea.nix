16367f6d0c953921532f064efc278632dbac5765
Ondřej Kvapil <git@viluon.me>
2026-04-08 11:10:30 +0200

    Revert "Remove explicit Wayland override"
    This reverts commit 6064f902b79e7222a4454b6278889efd56bbf8bf.

--- modules/editors/idea.nix
+++ modules/editors/idea.nix
@@ -10,8 +10,10 @@ _:
     {
       home.packages = [ idea ];
 
+      # run on Wayland
       xdg.configFile."JetBrains/IntelliJIdea${version-suffix}/idea64.vmoptions".text = ''
         -Xmx6144m
+        -Dawt.toolkit.name=WLToolkit
         -Dsun.java2d.vulkan=true
       '';
     };

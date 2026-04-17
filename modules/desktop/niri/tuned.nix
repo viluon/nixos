dc18a47b7d6c7cab428b0ef38796cf0c6e0b195e
Ondřej Kvapil <git@viluon.me>
2026-04-17 08:30:07 +0200

    Tune Niri to the target architecture

--- modules/desktop/niri/tuned.nix
+++ modules/desktop/niri/tuned.nix
@@ -1,11 +1,22 @@
 { pkgs
 , hostname
+, niri
 , ...
 }:
 let
+  rust-overlay = niri.inputs."xwayland-satellite-unstable".inputs."rust-overlay".overlays.default;
+  rust-nightly-pkgs = pkgs.extend rust-overlay;
+  rust-nightly = rust-nightly-pkgs."rust-bin".selectLatestNightlyWith (toolchain: toolchain.minimal);
+  rust-nightly-platform = pkgs.makeRustPlatform {
+    cargo = rust-nightly;
+    rustc = rust-nightly;
+  };
+
   optimised-niri =
-    pkgs.niri-unstable.overrideAttrs (new: old: {
-      env.RUSTFLAGS = old.env.RUSTFLAGS + " -Ctarget-cpu=${target-cpu} ";
+    (pkgs.niri-unstable.override {
+      rustPlatform = rust-nightly-platform;
+    }).overrideAttrs (_: old: {
+      env.RUSTFLAGS = old.env.RUSTFLAGS + " -Ctarget-cpu=${target-cpu} -Z tune-cpu=${tune-cpu} ";
     });
 
   target-cpu = "x86-64-v3";

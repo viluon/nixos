0a76b833a876e06b0e27039608629aab2e36ef8a
Ondřej Kvapil <git@viluon.me>
2026-06-25 18:39:01 +0200

    Deploy Grafana stack on nixboerse

--- hosts/nixboerse/default.nix
+++ hosts/nixboerse/default.nix
@@ -27,6 +27,8 @@ in
       ../../modules/desktop/stylix
       ../../modules/hardware/audio.nix
       ../../modules/hardware/graphics.nix
+      ../../modules/system/monitoring/governor-control.nix
+      ../../modules/system/monitoring/grafana-config.nix
       ../../modules/system/networking
       ../../modules/system/nix
       ../../modules/system/systemd

c9b221d5e5d646a5621a8d21d486df6446c68762
Ondřej Kvapil <git@viluon.me>
2026-08-06 14:04:23 +0200

    Fix access to Google Cloud Console

--- denix/modules/programs/firefox.nix
+++ denix/modules/programs/firefox.nix
@@ -4,13 +4,20 @@ delib.module {
 
   nixos.always.imports = [
     (
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
       {
         programs.firefox = {
           enable = true;
           package = pkgs.firefox-devedition;
           preferences = {
             "browser.tabs.allow_transparent_browser" = true;
+
+            "network.lna.skip-domains" = lib.concatStringsSep "," [
+              "googleapis.com"
+              "*.googleapis.com"
+              "*.gstatic.com"
+              "*.clients6.google.com"
+            ];
           };
         };
       }

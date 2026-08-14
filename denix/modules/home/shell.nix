235221bcc6256f84d5af527dde4c2aa469c04213
Ondřej Kvapil <git@viluon.me>
2026-08-14 13:33:02 +0200

    Revert "Redraw prompt periodically"
    This reverts commit c1513fef6c8471bc797b08ebac632d13116ef2ca.

--- denix/modules/home/shell.nix
+++ denix/modules/home/shell.nix
@@ -114,10 +114,6 @@ delib.module {
               fi
             }
 
-            # Redraw prompt every 30s
-            TMOUT=30
-            TRAPALRM() { zle && zle reset-prompt }
-
             # VS Code shell integration
             [[ "$TERM_PROGRAM" == "vscode" ]] && . "$(code --locate-shell-integration-path zsh)"
           '';
